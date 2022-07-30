0b7fc28b9f5e69f1498a75d1266e3ce4c5f60a79
Daniyar Kakimbekov <81741552+everypidigit@users.noreply.github.com>
2022-07-30 13:04:21 +0600

    Add files via upload

---
+++ mps_checker.py
@@ -0,0 +1,35 @@
+import torch as torch
+
+print(f"Is MPS (Metal Performance Shader) built? {torch.backends.mps.is_built()}")
+print(f"Is MPS available? {torch.backends.mps.is_available()}")
+
+def get_default_device():
+    """Pick MPS if available, else CPU"""
+    if torch.backends.mps.is_available():
+        return torch.device('mps')
+    else:
+        return torch.device('cpu')
+    
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+        
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl: 
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
+    
+device = get_default_device()
+device
